7388979da7d43d7974aa20c19881245e156ca9f8
cubasve <eva.cubasvasquez@gmail.com>
2021-02-10 20:20:48 -0500

    Add questions 12-16 with explanations

--- questions.js
+++ questions.js
@@ -83,3 +83,58 @@ EXPLANATION:
 - this = myFunction --> if the function is a class or object
 - this = undefined --> when 'strict' mode is used
 */
+
+//12. In what order will the numbers 1-4 be logged to the console when the code below is executed?
+(function() {
+    console.log(1);
+    setTimeout(function() {
+      console.log(2);
+    }, 1000);
+    setTimeout(function() {
+      console.log(3);
+    }, 0); //will wait until call stack is cleared
+    console.log(4);
+})();
+/* 1, 4, 3, 2 */
+/*
+EXPLANATION:
+- 1 & 4 are synchronous so they get logged one after the other
+- 2 & 3 are asynchronous so they get logged after their set time
+NOTE: Even though 3 is told to wait 0 seconds, it has to wait until the call stack is cleared before it is executed
+*/
+
+//13. What is the return of console.log?
+const n = 5;
+console.log(1..n); /* undefined */
+/*
+EXPLANATION:
+1. = is a valid number
+second dot: assumes you are using dot notation to access a property of the number you just created
+*/
+
+//14. What gets logged when we test the following equality scenarios?
+const a = c => c;
+const b = c => c;
+console.log(a == b); /* false */
+console.log(a(7) === b(7)); /* true */
+/*
+EXPLANANTION:
+*/
+
+//15. Let's display some notifications to our user! What gets logged in the snippet?
+const notifications = 1;
+console.log(`You have ${notifications} notification${notifications !== 1 && 's'}`);
+/* You have 1 notificationfalse */
+/*
+EXPLANATION:
+ */
+
+//16. Given an array of fruits, after slice method is applied, what will the result be?
+var fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'];
+var citrus = fruits.slice(1, 3);
+console.log(citrus); //[Orange, Lemon]
+/*
+EXPLANATION:
+Slice method starts from 1 and UP TO 3 (NOT INCLUDING)
+*/
+
